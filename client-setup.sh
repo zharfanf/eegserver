870b50accebbcfa2814e9e0ae33127fa06d89364
zharfanf <zharfanf@gmail.com>
2023-04-03 13:42:59 +0700

    adding script to set up the client

---
+++ client-setup.sh
@@ -0,0 +1,3 @@
+pip install gdown
+conda env create -f eegclient_environment_configuration.yml
+python -m pip install brainflow
---
+++ run.sh
@@ -0,0 +1,3 @@
+sudo apt-get update
+wget https://repo.anaconda.com/miniconda/Miniconda3-py38_23.1.0-1-Linux-x86_64.sh
+bash ./Miniconda3-py38_23.1.0-1-Linux-x86_64.sh
