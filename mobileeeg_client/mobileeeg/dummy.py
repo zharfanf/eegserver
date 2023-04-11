cf0f3d5d705da97aefbbb2c9647ebe4c0b763313
zharfanf <zharfanf@gmail.com>
2023-04-11 11:16:23 +0700

    updating content

--- mobileeeg_client/mobileeeg/dummy.py
+++ mobileeeg_client/mobileeeg/dummy.py
@@ -2,9 +2,10 @@ import argparse
 import time
 import numpy as np
 import random
+import requests
 
 def sendDataToServer(eegdata,acceldata,timestampdata,fsampling):
-	url='https://mobileeeg.yzd.my.id/api.php'
+	url='http://localhost/api.php'
 	apikey='68d79521339f81b3c90c2a8d631bda5f'
 	#print(timestampdata[0])
 	starttimestamp=timestampdata[0]
@@ -12,10 +13,10 @@ def sendDataToServer(eegdata,acceldata,timestampdata,fsampling):
 	datapoints=len(timestampdata)
 	waveformdata=np.transpose(np.vstack((timestampdata,eegdata,acceldata)))
 	jsondata = {'apikey':apikey, 'waveformdata': waveformdata.tolist(),'starttimestamp': starttimestamp, 'endtimestamp':endtimestamp,'datapoints':datapoints,'samplingfreq':fsampling}
-	response = requests.post(url, json=jsondata,timeout=0.001)
+	response = requests.post(url, json=jsondata,timeout=5)
 	print(response.text)
 
-eeg_data=[5,6,7,8,9]
+eeg_data=[1990,62310,71230,8410,920]
 timestamp_data=[0,1,2,3,4]
 accel_data=[10,11,12,13,14]
 fs=1200
