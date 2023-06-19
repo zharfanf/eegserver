821ae91e631a31c70d31368235456a5fa96d6ebb
Zaidan Nabhan <naufalz6@Zaidans-MacBook-Air.local>
2023-06-19 14:55:47 +0700

    Development 19-06-23

--- mobileeeg_client/mobileeeg/dummy.py
+++ mobileeeg_client/mobileeeg/dummy.py
@@ -5,19 +5,25 @@ import random
 import requests
 
 def sendDataToServer(eegdata,acceldata,timestampdata,fsampling):
-	url='http://192.168.82.175/api.php'
+	url='http://192.168.97.175/api.php'
 	apikey='68d79521339f81b3c90c2a8d631bda5f'
 	#print(timestampdata[0])
 	starttimestamp=timestampdata[0]
 	endtimestamp=timestampdata[-1]
 	datapoints=len(timestampdata)
-	waveformdata=np.transpose(np.vstack((timestampdata,eegdata,acceldata)))
+	waveformdata=[]
+	for i in range(len(eegdata)):
+		waveformdata = np.transpose(np.vstack((timestampdata,eegdata[i],acceldata)))
+	print(waveformdata.tolist())
 	jsondata = {'apikey':apikey, 'waveformdata': waveformdata.tolist(),'starttimestamp': starttimestamp, 'endtimestamp':endtimestamp,'datapoints':datapoints,'samplingfreq':fsampling}
 	response = requests.post(url, json=jsondata,timeout=5)
 	print(response.text)
 
-eeg_data=[1990,62310,71230,8410,920]
+eeg_data=[[21331,2323,142142,123123,124124],[21331,2323,142142,123123,124124]]
+# timestamp_data=[[0,1,2,3,4],[0,1,2,3,4],[0,1,2,3,4]]
+# accel_data=[[10,11,12,13,14],[10,11,12,13,14],[10,11,12,13,14]]
+# eeg_data=[1990,62310,71230,8410,920]
 timestamp_data=[0,1,2,3,4]
-accel_data=[10,11,12,13,14]
+accel_data=[14,21,44,12,32]
 fs=1200
 sendDataToServer(eeg_data,accel_data,timestamp_data,fs)
