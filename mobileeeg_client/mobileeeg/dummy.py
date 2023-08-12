17b6d111e724362a7bdd61cb101ec37da2d252c3
Zaidan Nabhan <18119005@std.stei.itb.ac.id>
2023-08-12 23:06:24 +0700

    compare code

--- mobileeeg_client/mobileeeg/dummy.py
+++ mobileeeg_client/mobileeeg/dummy.py
@@ -5,13 +5,8 @@ import random
 import requests
 
 def sendDataToServer(eegdata,acceldata,timestampdata,fsampling):
-<<<<<<< HEAD
 	url='http://172.168.101.129/api.php'
 	apikey='c5a2b2c1ca14c5e7533e5548295a3d3b'
-=======
-	url='http://192.168.97.175/api.php'
-	apikey='68d79521339f81b3c90c2a8d631bda5f'
->>>>>>> 25c1796fd0b62328d76b08972376b1b01ef43e71
 	#print(timestampdata[0])
 	starttimestamp=timestampdata[0]
 	endtimestamp=timestampdata[-1]
@@ -24,16 +19,9 @@ def sendDataToServer(eegdata,acceldata,timestampdata,fsampling):
 	response = requests.post(url, json=jsondata,timeout=5)
 	print(response.text)
 
-<<<<<<< HEAD
 
 
 eeg_data=[1990,62310,71230,8410,920]
-=======
-eeg_data=[[21331,2323,142142,123123,124124],[21331,2323,142142,123123,124124]]
-# timestamp_data=[[0,1,2,3,4],[0,1,2,3,4],[0,1,2,3,4]]
-# accel_data=[[10,11,12,13,14],[10,11,12,13,14],[10,11,12,13,14]]
-# eeg_data=[1990,62310,71230,8410,920]
->>>>>>> 25c1796fd0b62328d76b08972376b1b01ef43e71
 timestamp_data=[0,1,2,3,4]
 accel_data=[14,21,44,12,32]
 fs=1200
