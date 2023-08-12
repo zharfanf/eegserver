bbba1c48cf243b1e9479066476162c44d62d4d69
Zaidan Nabhan <18119005@std.stei.itb.ac.id>
2023-08-12 22:57:29 +0700

    idk

--- mobileeeg_client/mobileeeg/dummy.py
+++ mobileeeg_client/mobileeeg/dummy.py
@@ -5,8 +5,8 @@ import random
 import requests
 
 def sendDataToServer(eegdata,acceldata,timestampdata,fsampling):
-	url='http://192.168.82.175/api.php'
-	apikey='68d79521339f81b3c90c2a8d631bda5f'
+	url='http://172.168.101.129/api.php'
+	apikey='c5a2b2c1ca14c5e7533e5548295a3d3b'
 	#print(timestampdata[0])
 	starttimestamp=timestampdata[0]
 	endtimestamp=timestampdata[-1]
@@ -15,6 +15,8 @@ def sendDataToServer(eegdata,acceldata,timestampdata,fsampling):
 	jsondata = {'apikey':apikey, 'waveformdata': waveformdata.tolist(),'starttimestamp': starttimestamp, 'endtimestamp':endtimestamp,'datapoints':datapoints,'samplingfreq':fsampling}
 	response = requests.post(url, json=jsondata,timeout=5)
 	print(response.text)
+
+
 
 eeg_data=[1990,62310,71230,8410,920]
 timestamp_data=[0,1,2,3,4]
