980b04d67a93387ebd61e1830512effc98ffce12
Zaidan Nabhan <18119005@std.stei.itb.ac.id>
2023-08-14 13:57:42 +0700

    interfacing to web

--- mobileeeg_client/mobileeeg/eegRun.py
+++ mobileeeg_client/mobileeeg/eegRun.py
@@ -4,47 +4,55 @@ import numpy as np
 import random
 import requests
 
-data_array = []
-data_arrays = []
-array_random = []
-waveformdata=[]
-data_array_2d=[]
-
 def readFile (data_array,file_path):
     f = open(file_path, 'r')
     lines = f.readlines()
     data_array = np.array([float(line.strip()) for line in lines])
     return data_array
 
-def randomArray (array_random) :
+def randomArraydata (array_random_file) :
 	for i in range (1,9) :
 		x = random.randint(1,100)
-		array_random.append(x)
-	return array_random
-	
+		array_random_file.append(x)
+	return array_random_file
 
-def sendDataToServer(data_arrays_2d):
-	# url='http://192.168.97.175/api.php'
-	# apikey='68d79521339f81b3c90c2a8d631bda5f'
-	for i in range(len(data_arrays_2d)):
-		waveformdata = np.transpose(np.vstack(data_arrays_2d))
+def randomArraytime (starttimestamp) :
+	# 25 detik, 4096 data -> 25/4096
+	for i in range (4097) :
+		timestampdata.append(starttimestamp + (i * (25/4097)))
+	return timestampdata
+
+def sendDataToServer(data_array_2d,timestampdata):
+	url='http://192.168.175.212/api.php'
+	apikey='ff5f9ee86c6e510749a024415eb05a14'
+	datapoints=4096
+	starttimestamp=timestampdata[0]
+	endtimestamp=timestampdata[-1]
+	waveformdata = np.transpose(np.vstack((timestampdata, data_array_2d)))
 	print(waveformdata.tolist())
-	# jsondata = {'apikey':apikey, 'waveformdata': waveformdata.tolist(),'starttimestamp': starttimestamp, 'endtimestamp':endtimestamp,'datapoints':datapoints,'samplingfreq':fsampling}
-	# response = requests.post(url, json=jsondata,timeout=5)
-	# print(response.text)
+	jsondata = {'apikey':apikey, 'waveformdata': waveformdata.tolist(),'starttimestamp': starttimestamp, 'endtimestamp':endtimestamp,'datapoints':datapoints,'samplingfreq':174}
+	response = requests.post(url, json=jsondata,timeout=5)
+	print(response.text)
 
-array_random=randomArray(array_random)
-for i in array_random :
-	if (i<10) :
-		file_path = f'Z/Z00{i}.txt'
-	elif (i<100) :
-		file_path = f'Z/Z0{i}.txt'
-	else :
-		file_path = f'Z/Z{i}.txt'
-	data_array = readFile(data_array,file_path)
-	data_arrays.append(data_array)
-
-
-data_array_2d = np.array(data_arrays)
-# print(data_array_2d)
-sendDataToServer(data_array_2d)
+while True :
+	data_array = []
+	data_arrays = []
+	timestampdata = []
+	array_random_file = []
+	waveformdata=[]
+	data_array_2d=[]
+	array_random_file=randomArraydata(array_random_file)
+	timestampdata=randomArraytime(time.time())
+	for i in array_random_file :
+		if (i<10) :
+			file_path = f'Z/Z00{i}.txt'
+		elif (i<100) :
+			file_path = f'Z/Z0{i}.txt'
+		else :
+			file_path = f'Z/Z{i}.txt'
+		data_array = readFile(data_array,file_path)
+		data_arrays.append(data_array)
+	data_array_2d = np.array(data_arrays)
+	# print(data_array_2d)
+	sendDataToServer(data_array_2d, timestampdata)
+	time.sleep(60)
